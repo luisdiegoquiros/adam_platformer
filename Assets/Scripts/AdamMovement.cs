70e3ea9df172a386b959045fc217723709cddd27
Luis Diego Quirós Gómez <luis25quiros@gmail.com>
2024-05-20 00:17:12 -0600

    player simple jump
    includes jump logic and the raycasting to the bottom

--- Assets/Scripts/AdamMovement.cs
+++ Assets/Scripts/AdamMovement.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class AdamMovement : MonoBehaviour
 {
+    public LayerMask groundLayer;
+    public float rayLength = 1.5f;
 
     private Rigidbody2D myRigidBody;
 
     public float playerSpeed = 8f;
+    public float jumpSpeed = 8f;
 
     Vector2 moveInput;
 
@@ -38,4 +42,24 @@ public class AdamMovement : MonoBehaviour
         myRigidBody.velocity = playerVelocity;
     }
 
+    private void OnJump(InputValue value)
+    {
+        if (value.isPressed & IsGrounded())
+        {
+            myRigidBody.velocity += new Vector2(0f, jumpSpeed);
+
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        // Cast a ray straight down.
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, distance: rayLength, layerMask: groundLayer);
+
+
+
+        Debug.DrawRay(this.transform.position, Vector2.down * rayLength, Color.red, duration:1f);
+
+        return hit.collider != null;
+    }
 }
